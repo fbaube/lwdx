6e35915a173bcbecc8eaab089320702e465ff483
Fred Baube <fbaube@welho.com>
2023-02-02 21:35:07 +0200

    Add tag-triplets for(XDITA,HDITA,MDITA)

---
+++ equivalents.go
@@ -0,0 +1,17 @@
+package lwdx
+
+type Equivs struct {
+	Xdita, Hdita, Mdita string
+}
+
+// Equivalents is an imperfect attampt to correlate tags across
+// LwDITA languages.
+//
+// Note that for MDITA, the "Mdita" value is from the Goldmark parser
+// library, and therefore (to take one example), "Document" means that
+// for a node in question, its property [goldmark.ast.NodeKind] has the
+// value [goldmark.ast.KindDocument].
+// .
+var Equivalents = []Equivs{
+	{"topic", "html", "Document"},
+}
